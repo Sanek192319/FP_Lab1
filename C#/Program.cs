af106d8284654b1197097e8314897a0a25808087
Sanek192319 <71784772+Sanek192319@users.noreply.github.com>
2022-10-03 19:09:41 +0300

    Updare Program.cs

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,32 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace Lab1
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Singleton");
-            List<int> elements = Singleton<int>(0);
-            Console.WriteLine(elements[0]);
-            Console.WriteLine("Null");
-            bool b = Null<int>(elements);
-            Console.WriteLine(b);
-            Console.WriteLine("snoc");
-            Snoc<int>(elements, 10);
-            foreach (var item in elements)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine("length");
-            int k = Length(elements);
-            Console.WriteLine(k);
-        }
         public static List<T> Singleton<T>(T element)
         {
             return new List<T> { element };
         }
+
         public static bool Null<T>(List<T> elements)
             {
             int k = 0;
@@ -36,6 +12,7 @@ namespace Lab1
             }
             return k == 0 ? true : false;
             }
+
         public static void Snoc<T>(List<T> elements, T element) => elements.Add(element);
 
         public static int Length<T>(List<T> elements)
@@ -47,5 +24,4 @@ namespace Lab1
             }
             return k;
         }
-    }
-}
+ 
